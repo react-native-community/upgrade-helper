4f05b53819478a8527ae9f94f4a6271fa033e9c0
Pavlos Vinieratos <pvinis@gmail.com>
2019-03-13 21:12:07 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Reactaa
           </a>
         </header>
       </div>
