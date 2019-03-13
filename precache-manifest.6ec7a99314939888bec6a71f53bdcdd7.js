537b900f6d8d26ff4f48487aba53432ca49ed652
Pavlos Vinieratos <pvinis@gmail.com>
2019-03-13 20:54:53 +0100

    Updates

---
+++ precache-manifest.6ec7a99314939888bec6a71f53bdcdd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a23f3bedf33a8bb8ae75",
+    "url": "/pvinis/purge-web/static/css/main.2378bba7.chunk.css"
+  },
+  {
+    "revision": "a23f3bedf33a8bb8ae75",
+    "url": "/pvinis/purge-web/static/js/main.9b7ee3a2.chunk.js"
+  },
+  {
+    "revision": "7575a6cfe0ded8d03539",
+    "url": "/pvinis/purge-web/static/js/runtime~main.7b95089d.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/pvinis/purge-web/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pvinis/purge-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cd7ce82102f3aba39ba06c6f444316be",
+    "url": "/pvinis/purge-web/index.html"
+  }
+];
