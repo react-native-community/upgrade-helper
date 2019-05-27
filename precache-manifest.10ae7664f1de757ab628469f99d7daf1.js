f49c6903cb302208798241739d6dfa96395ff6dd
Pavlos Vinieratos <pvinis@gmail.com>
2019-05-27 16:01:25 +0200

    Updates

---
+++ precache-manifest.10ae7664f1de757ab628469f99d7daf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9a89501109be53e4f543946a0d5f47",
+    "url": "/purge-web/index.html"
+  },
+  {
+    "revision": "6cf90197522f6b9fd049",
+    "url": "/purge-web/static/css/main.5f88173a.chunk.css"
+  },
+  {
+    "revision": "7d277834241e389c85ff",
+    "url": "/purge-web/static/js/2.a1676396.chunk.js"
+  },
+  {
+    "revision": "6cf90197522f6b9fd049",
+    "url": "/purge-web/static/js/main.a3b98bd6.chunk.js"
+  },
+  {
+    "revision": "ac0e094b698dbf50cc58",
+    "url": "/purge-web/static/js/runtime~main.1fbd653a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/purge-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/purge-web/precache-manifest.3e3fb30b4f71974846ff9dd13d7f8a8b.js"
+  "/purge-web/precache-manifest.10ae7664f1de757ab628469f99d7daf1.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/purge-web/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/purge-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
