ac34e8388392fcb4a918e40e80570d9f631f8acd
Lucas Bento <lucas.bsilva@outlook.com>
2019-05-31 10:12:27 +0200

    Use antd.css file instead of index.css

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import 'antd/dist/antd.css';
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
