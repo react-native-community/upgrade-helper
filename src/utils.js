cdb7c61cc91bb79efe43d3c1080ad0c70f471ff0
Lucas Bento <lucas.bsilva@outlook.com>
2019-06-07 12:24:57 +0200

    Remove unused variables

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,3 @@
-export const INSTRUCTION_PLATFORMS = {
-  IOS: 'iOS',
-  ANDROID: 'Android'
-}
-
-export const INSTRUCTION_CATEGORIES = {
-  ADDED: 'Added',
-  WARNINGS: 'Warnings',
-  CHANGES: 'Changes',
-  BREAKING_CHANGES: 'Breaking Changes',
-  TROUBLESHOOTING: 'Troubleshooting'
-}
-
-export const RELEASED_VERSIONS = ['0.59.0']
-
 export const RELEASES_URL =
   'https://raw.githubusercontent.com/react-native-community/rn-diff-purge/master/RELEASES'
 
-export const PACKAGE_MANAGERS = {
-  yarn: {
-    command: pkg => `yarn add ${pkg}`
-  },
-  npm: {
-    command: pkg => `npm install ${pkg} --save`
-  }
-}
