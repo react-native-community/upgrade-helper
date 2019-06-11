bcee9e0471b565a129eada5aec03563800b6b86b
Lucas Bento <lucas.bsilva@outlook.com>
2019-06-11 11:25:06 +0200

    Move function to filter versions based on the diff requirements to `utils`

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,21 @@
+import semver from 'semver'
+import { versions } from './releases'
+
+const releasedVersions = versions.map(version => ({
+  ...require(`./releases/${version}`).default,
+  version
+}))
+
 export const RELEASES_URL =
   'https://raw.githubusercontent.com/react-native-community/rn-diff-purge/master/RELEASES'
 
 export const removeAppPathPrefix = path => path.replace(/RnDiffApp\//, '')
+
+export const getVersionsInDiff = ({ fromVersion, toVersion }) => {
+  const cleanedToVersion = semver.valid(semver.coerce(toVersion))
+
+  return releasedVersions.filter(
+    ({ version }) =>
+      semver.lte(version, cleanedToVersion) && semver.gt(version, fromVersion)
+  )
+}
