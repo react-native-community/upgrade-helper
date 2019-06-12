cc3673ad9a9ae0384db7f3a4241b3342149363f9
Lucas Bento <lucas.bsilva@outlook.com>
2019-06-12 16:52:15 +0200

    Fix issue with new binary files not being identified as _added_

--- src/components/common/DiffViewer.js
+++ src/components/common/DiffViewer.js
@@ -60,6 +60,8 @@ const DiffViewer = ({
         <Diff
           key={`${diff.oldRevision}${diff.newRevision}`}
           {...diff}
+          // otakustay/react-diff-view#49
+          type={diff.type === 'new' ? 'add' : diff.type}
           selectedChanges={selectedChanges}
           onToggleChangeSelection={onToggleChangeSelection}
         />
