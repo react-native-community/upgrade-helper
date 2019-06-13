7765b88956a23c1072ed19acfc4b914212f45dce
Lucas Bento <lucas.bsilva@outlook.com>
2019-06-13 15:41:25 +0200

    Add star on GitHub button

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Card } from 'antd'
+import GitHubButton from 'react-github-btn'
 import VersionSelector from '../common/VersionSelector'
 import DiffViewer from '../common/DiffViewer'
 
@@ -17,6 +18,20 @@ const Container = styled(Card)`
   border-radius: 3px;
 `
 
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const StarButton = styled(({ className, ...props }) => (
+  <div className={className}>
+    <GitHubButton {...props} />
+  </div>
+))`
+  margin-bottom: 5px;
+  margin-left: 15px;
+`
+
 const Home = () => {
   const [fromVersion, setFromVersion] = useState('')
   const [toVersion, setToVersion] = useState('')
@@ -31,6 +46,19 @@ const Home = () => {
   return (
     <Page>
       <Container>
+        <TitleContainer>
+          <h1>React Native upgrade guide</h1>
+
+          <StarButton
+            href="https://github.com/react-native-community/upgrade-helper"
+            data-icon="octicon-star"
+            data-show-count="true"
+            aria-label="Star react-native-community/upgrade-helper on GitHub"
+          >
+            Star
+          </StarButton>
+        </TitleContainer>
+
         <VersionSelector showDiff={handleShowDiff} />
       </Container>
 
