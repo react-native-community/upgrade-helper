dbd680c99bdf03bf04167f9b1883a1f2ea9c8d5d
Pavlos Vinieratos <pvinis@gmail.com>
2019-07-03 16:11:10 +0200

    try

--- src/releases/0.60.js
+++ src/releases/0.60.js
@@ -35,9 +35,13 @@ export default {
             For an easy upgrade experience of this file, do the following.
             After all the other files have been upgraded, run `yarn install` and `cd ios; pod install; cd ..`.
             This will cause all the Pods to be connected. After that, open the workspace file in the `ios` directory, called something like `MyApp.xcworkspace`
+            ![image](public/images/60-1.png)
             This workspace contains your original project, as well as the `Pods` project. Expand your project and find the `Libraries` directory.
+            ![image](public/images/60-2.png)
             Inside that, there are a few libraries related to React Native, and possibly other native modules from other libraries you might have connected.
+            ![image](public/images/60-3.png)
             You can go ahead and remove the React Native related libraries from there, by selecting them and hitting the delete button. In the popup you get, you can select `Remove references`. If you select to `Move it to Trash`, you might need to run `yarn install` again.
+            ![image](public/images/60-4.png)
             These libraries are not needed there anymore, since now they are connected using CocoaPods. You can do the same for other libraries that might exist in that directory, if they support CocoaPods. If they don't, keep them there for now.
             After all this, your diff of that file should look roughly the same as the one right here. Don't worry too much if it doesn't match exactly, as your project might have more or fewer targets.
             Try to see if the rough outline is the same, and of course make sure your project can build and run.
