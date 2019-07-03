e29a4c5e2e8e545cce6b356e81949ffeb8cc7fd6
Pavlos Vinieratos <pvinis@gmail.com>
2019-07-03 16:02:48 +0200

    updates

--- src/releases/0.60.js
+++ src/releases/0.60.js
@@ -7,15 +7,10 @@ export default {
     links: [
       {
         title:
-          'Official blog post about the major changes on React Native 0.59',
+          'Official blog post about the major changes on React Native 0.60',
         url:
-          'https://facebook.github.io/react-native/blog/2019/03/12/releasing-react-native-059'
+          'https://facebook.github.io/react-native/blog/2019/07/03/version-60'
       },
-      {
-        title: 'Tutorial on upgrading to React Native 0.59',
-        url:
-          'https://reactnative.thenativebits.com/courses/upgrading-react-native/upgrade-to-react-native-0.59/'
-      }
     ],
   },
   comments: [
@@ -27,6 +22,25 @@ export default {
         <Fragment>
           All these libraries below have been removed from the Xcode project file and now live in the Podfile.
           Cocoapods handles the linking now.
+          Here you can add more libraries with native modules.
+        </Fragment>
+        )
+      },
+    {
+      fileName: 'ios/RnDiffApp.xcodeproj/project.pbxproj',
+      lineNumber: 9,
+      lineChangeType: 'neutral',
+      content: (
+        <Fragment>
+            For an easy upgrade experience of this file, do the following.
+            After all the other files have been upgraded, run `yarn install` and `cd ios; pod install; cd ..`.
+            This will cause all the Pods to be connected. After that, open the workspace file in the `ios` directory, called something like `MyApp.xcworkspace`
+            This workspace contains your original project, as well as the `Pods` project. Expand your project and find the `Libraries` directory.
+            Inside that, there are a few libraries related to React Native, and possibly other native modules from other libraries you might have connected.
+            You can go ahead and remove the React Native related libraries from there, by selecting them and hitting the delete button. In the popup you get, you can select `Remove references`. If you select to `Move it to Trash`, you might need to run `yarn install` again.
+            These libraries are not needed there anymore, since now they are connected using CocoaPods. You can do the same for other libraries that might exist in that directory, if they support CocoaPods. If they don't, keep them there for now.
+            After all this, your diff of that file should look roughly the same as the one right here. Don't worry too much if it doesn't match exactly, as your project might have more or fewer targets.
+            Try to see if the rough outline is the same, and of course make sure your project can build and run.
         </Fragment>
         )
       },
