e3a3fda52a8905563c8638ec8edfda1fa93d987e
Lucas Bento <lucas.bsilva@outlook.com>
2019-08-14 17:39:30 +0200

    Remove unused CSS properties & change color of loading a little bit

--- src/components/common/Diff/DiffLoading.js
+++ src/components/common/Diff/DiffLoading.js
@@ -3,12 +3,12 @@ import styled from 'styled-components'
 import ContentLoader from 'react-content-loader'
 
 const TitleLoader = () => (
-  <ContentLoader speed={1} primaryColor="#b3b3b3" secondaryColor="#e6e6e6">
-    <rect rx="3" ry="3" width="100" height="7" />{' '}
+  <ContentLoader speed={1} primaryColor="#eee" secondaryColor="#e6e6e6">
+    <rect rx="1" ry="1" width="100" height="3" />{' '}
   </ContentLoader>
 )
 const DiffLoader = () => (
-  <ContentLoader speed={1} primaryColor="#b3b3b3" secondaryColor="#e6e6e6">
+  <ContentLoader speed={1} primaryColor="#eee" secondaryColor="#e6e6e6">
     <rect x="0" y="7" rx="4" ry="4" width="90%" height="13" />{' '}
     <rect x="0" y="30" rx="4" ry="4" width="80%" height="13" />{' '}
     <rect x="0" y="53" rx="3" ry="3" width="70%" height="10" />
@@ -19,30 +19,22 @@ const DiffLoader = () => (
 )
 
 const SkeletonContainer = styled.div`
-  position: relative;
   margin-top: 16px;
-  background-color: 'white';
   border: 1px solid #e8e8e8;
   border-radius: 3px;
-  max-height: '800px';
-  overflow: hidden;
 `
 
 const Header = styled.div`
   color: #24292e;
-  line-height: 32px;
   background-color: #fafbfc;
-  border: 1px solid #e8e8e8;
-  padding: 10px 5px;
+  padding: 14px 10px;
   height: 40px;
 `
 
 const DiffDisplay = styled.div`
-  border-radius: 3px;
   padding: 5px 10px;
   height: 400px;
   column-count: 2;
-  align-items: center;
 `
 export default class UsefulContentNoData extends Component {
   render() {
