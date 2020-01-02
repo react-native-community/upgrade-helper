458d77788d18c2e7da3754c16410e603bec96c0f
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-01-02 23:36:18 +0900

    feat: use markdown and change message

--- src/components/common/AppNameWarning.js
+++ src/components/common/AppNameWarning.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Alert } from 'antd'
+import Markdown from './Markdown'
 
 const Container = styled.div({
-  width: '100%'
+  width: '100%',
+  marginTop: '16px'
 })
 
 export const AppNameWarning = () => (
   <Container>
     <Alert
-      message="Don't forget: `RnDiffApp` is a placeholder. When upgrading, all
-          instances of `RnDiffApp` should be `YourProjectName`, all `rndiffapp`
-          should be `yourprojectname` etc."
+      message={
+        <Markdown>
+          Keep in mind that `RnDiffApp` and `rndiffapp` are placeholders. When
+          upgrading, you should replace them with your actual project's name.
+        </Markdown>
+      }
       type="info"
       closable
     />
