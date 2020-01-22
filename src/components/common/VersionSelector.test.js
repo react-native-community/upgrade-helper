c0e8cdff91e72316b9eeb4a23c7b5f7cb3ff5781
Pavlos Vinieratos <pvinis@gmail.com>
2020-01-22 07:55:21 +0100

    first try

---
+++ src/components/common/VersionSelector.test.js
@@ -0,0 +1,45 @@
+import R from 'ramda'
+
+import { filterReleases, isRC } from './VersionSelector'
+
+describe('rc checker', () => {
+  it('should detect rc releases correctly', () => {
+    expect(isRC('0.62.0')).toBe(false)
+    expect(isRC('0.62.0-rc.0')).toBe(true)
+  })
+})
+
+const releases = [
+  '0.63.2-rc.1',
+  '0.63.2-rc.0',
+  '0.63.1',
+  '0.63.0',
+  '0.62.4',
+  '0.62.4-rc.1',
+  '0.62.4-rc.0',
+  '0.62.3',
+  '0.62.1',
+  '0.62.0',
+  '0.62.0-rc.1',
+  '0.61.3'
+]
+const releasesNoRCs = R.reject(isRC)(releases)
+// console.log(releasesNoRCs)
+const releasesNoRCsWithLatestReleaseRCs = [
+  '0.63.2-rc.1',
+  '0.63.2-rc.0',
+  ...releasesNoRCs
+]
+
+describe('release filtering', () => {
+  it('should return the correct releases', () => {
+    // expect(filterReleases(releasesWithRCs)).toBe(releasesWithRCs)
+    expect(filterReleases(releases, {})).toEqual(releases)
+
+    expect(filterReleases(releases, { showRCs: 'all' })).toEqual(releases)
+    expect(filterReleases(releases, { showRCs: 'none' })).toEqual(releasesNoRCs)
+    expect(filterReleases(releases, { showRCs: 'latest' })).toEqual(
+      releasesNoRCsWithLatestReleaseRCs
+    )
+  })
+})
