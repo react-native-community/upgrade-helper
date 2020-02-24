78a2367fe2dc9d2dc2484f58a30f80e80cec5a6b
Pavlos Vinieratos <pvinis@gmail.com>
2020-02-24 14:39:52 +0400

    remove unused

--- src/__tests__/components/common/Loading.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import Loading from '../../../components/common/Loading'
-
-it('renders without crashing', () => {
-  const { container } = render(<Loading logo="test.png" />)
-
-  expect(container).toMatchSnapshot()
-})
