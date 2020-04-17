897a73b54cc837e4f140eaeb8859bf981885ddfe
Lucas Bento <lucas.bsilva@outlook.com>
2020-04-17 13:08:44 +0200

    Fix spacing before `UpgradeSupportAlert`

--- src/components/common/UpgradeSupportAlert.js
+++ src/components/common/UpgradeSupportAlert.js
@@ -3,27 +3,29 @@ import styled from '@emotion/styled'
 import { Tooltip } from 'antd'
 
 const UpgradeSupportAlert = styled(props => (
-  <span {...props}>
-    Check out{' '}
-    <Tooltip
-      placement="bottom"
-      title="Upgrade Support is a community-backed place to request and give help when upgrading your app."
-    >
-      <a
-        href="https://github.com/react-native-community/upgrade-support"
-        target="_blank"
-        rel="noopener noreferrer"
-        {...props}
+  <div {...props}>
+    <span>
+      Check out{' '}
+      <Tooltip
+        placement="bottom"
+        title="Upgrade Support is a community-backed place to request and give help when upgrading your app."
       >
-        Upgrade Support
-      </a>
-    </Tooltip>{' '}
-    if you are experiencing issues related to React Native during the upgrading
-    process.
-  </span>
+        <a
+          href="https://github.com/react-native-community/upgrade-support"
+          target="_blank"
+          rel="noopener noreferrer"
+          {...props}
+        >
+          Upgrade Support
+        </a>
+      </Tooltip>{' '}
+      if you are experiencing issues related to React Native during the
+      upgrading process.
+    </span>
+  </div>
 ))`
   padding-top: 15px;
-  a {
+  span > a {
     color: #045dc1;
 
     &:hover {
