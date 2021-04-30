0a8faa3f30c261e1865562daff590e5ff8e9d64c
Miguel Aniceto <miguel.aniceto@mixcloud.com>
2021-04-30 09:51:26 +0100

    Add 0.64 to the versions on releases/index.js

--- src/releases/index.js
+++ src/releases/index.js
@@ -1,5 +1,5 @@
 // this line HAAAAAAAAS to go
-const versions = ['0.62', '0.61', '0.60', '0.59', '0.58', '0.57']
+const versions = ['0.64', '0.62', '0.61', '0.60', '0.59', '0.58', '0.57']
 
 export default versions.map(version => ({
   ...require(`./${version}`).default,
