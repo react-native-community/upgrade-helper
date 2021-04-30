f45ad22a161b0ef6267cc76ad25ac3c29fb1f55b
Miguel Aniceto <miguel.aniceto@mixcloud.com>
2021-04-30 09:51:08 +0100

    Add the release 0.64.js file that contains relevant information about upgrading to 0.64

---
+++ src/releases/0.64.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react'
+
+export default {
+  usefulContent: {
+    description:
+      'React Native 0.64 includes hermes optin-in on iOS and React 17.',
+    links: [
+      {
+        title:
+          'Official blog post about the major changes on React Native 0.62',
+        url: 'https://reactnative.dev/blog/2021/03/12/version-0.64'
+      }
+    ]
+  },
+  comments: [
+    {
+      fileName: 'package.json',
+      lineNumber: 14,
+      lineChangeType: 'add',
+      content: (
+        <Fragment>
+          If you have the `hermes-engine` dependency you need to upgrade to
+          0.7.2 [see release
+          here](https://github.com/facebook/hermes/releases/tag/v0.7.2) if you
+          are on a previous version you might get crashes at boot on Android.
+        </Fragment>
+      )
+    }
+  ]
+}
