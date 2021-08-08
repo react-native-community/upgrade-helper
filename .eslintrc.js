3bcb75f41d9f4879a5d191a822dc8fb66d9353c5
Lucas Bento <lucas.bsilva@outlook.com>
2021-08-08 20:55:06 +0200

    refactor: move ESLint config to `package.json`

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: 'react-app',
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'jsx-a11y/accessible-emoji': 'off',
-    'react-hooks/exhaustive-deps': 'off'
-  }
-}
