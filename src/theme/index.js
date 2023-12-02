4b1af3660ed4c6584b90c0eee7120bb78509e2b6
Lucas Bento <lucas.bsilva@outlook.com>
2023-12-02 17:00:10 +0100

    refactor: tweak the dark mode colors

--- src/theme/index.js
+++ src/theme/index.js
@@ -40,35 +40,35 @@ export const lightTheme = {
   },
 }
 export const darkTheme = {
-  background: '#363537',
+  background: '#0d1117',
 
   text: '#FAFAFA',
   textHover: '#999999',
   link: '#045dc1',
   linkHover: '#40a9ff',
 
-  border: '#555555',
+  border: '#30363d',
   greenBorder: '#bef5cb',
   yellowBorder: '#c69026',
-  yellowBackground: '#37332A',
+  yellowBackground: '#37332a8a',
 
   diff: {
     // Color object from https://github.com/otakustay/react-diff-view/blob/master/site/components/DiffView/diff.global.less
     textColor: '#fafafa',
     selectionBackground: '#5a5f80',
-    gutterInsertBackground: '#082525',
-    gutterDeleteBackground: '#2b1523',
+    gutterInsertBackground: '#3fb9504d',
+    gutterDeleteBackground: '#f8514c4d',
     gutterSelectedBackground: '#5a5f80',
-    codeInsertBackground: '#082525',
-    codeDeleteBackground: '#2b1523',
-    codeInsertEditBackground: '#00462f',
-    codeDeleteEditBackground: '#4e2436',
+    codeInsertBackground: '#2ea04326',
+    codeDeleteBackground: '#f851491a',
+    codeInsertEditBackground: '#2ea04366',
+    codeDeleteEditBackground: '#f8514c66',
     codeSelectedBackground: '#5a5f80',
     omitBackground: '#101120',
     decorationGutterBackground: '#222',
-    decorationGutter: '#ababab',
-    decorationContentBackground: '#222',
-    decorationContent: '#ababab',
+    decorationGutter: '#388bfd66',
+    decorationContentBackground: '#388bfd1a',
+    decorationContent: '#7d8590',
   },
 
   // Alternating Row Colors for Binary Download component and Content Loader animation
